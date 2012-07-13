8dadb8ee7b1e3b1bdfeffefe3503789de9fbb28e
Annie Lin <ecuoln@gmail.com>
2012-07-13 11:38:42 +0800

    Finish user signup

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,6 +11,7 @@
 
 class User < ActiveRecord::Base
   attr_accessible :email, :name, :password, :password_confirmation
+  
   has_secure_password
   
   validates :name, presence: true, length: { maximum: 50 }
